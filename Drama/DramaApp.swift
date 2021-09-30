14b3a0f1015a32612169abf3dee5ef8f69e6dd3e
dutchman <jewel@ticonsys.com>
2021-09-30 23:12:29 +0600

    Initial Commit

---
+++ Drama/DramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DramaApp.swift
+//  Drama
+//
+//  Created by dutchman on 30/9/21.
+//
+
+import SwiftUI
+
+@main
+struct DramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
