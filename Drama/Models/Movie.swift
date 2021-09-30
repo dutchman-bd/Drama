2f60304a2d592e8e01ab9e233fca8f7c73983fb4
dutchman <jewel@ticonsys.com>
2021-10-01 01:55:54 +0600

    MovieList Caroseal view

---
+++ Drama/Models/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  Drama
+//
+//  Created by dutchman on 30/9/21.
+//
+
+import Foundation
+
+struct MovieResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let backdropPath: String?
+    let posterPath: String?
+    let overview: String
+    let voteAverage: Double
+    let runtime: Int?
+    
+    var backdropURL: URL {
+        return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath ?? "")")!
+    }
+}
---
+++ Drama/Services/MovieService.swift
@@ -0,0 +1,53 @@
+//
+//  MovieService.swift
+//  Drama
+//
+//  Created by dutchman on 30/9/21.
+//
+
+import Foundation
+
+protocol MovieService {
+    func fetchMovies(from endpoint: MoviesEndPoint, completion: @escaping (Result<MovieResponse, MovieError>) -> ())
+    func fetchMovie(id: Int, completion: @escaping (Result<Movie, MovieError>) -> ())
+    func searchMovies(query: String, completion: @escaping (Result<MovieResponse, MovieError>) ->())
+}
+
+
+enum MoviesEndPoint: String {
+    case nowPlaying = "now_playing"
+    case upcoming
+    case topRated = "top_rated"
+    case popular
+    
+    var description: String {
+        switch self {
+            case .nowPlaying: return "Now Playing"
+            case .upcoming: return "Upcoming"
+            case .topRated: return "Top Rated"
+            case .popular: return "Popular"
+        }
+    }
+}
+
+
+enum MovieError: Error, CustomNSError {
+    case apiError
+    case invalidEndpoint
+    case invalidResponse
+    case noData
+    case serializationError
+    
+    var localizedDescription: String {
+        switch self {
+            case .apiError: return "Failed to fetch data"
+            case .invalidEndpoint: return "Invalid endpoint"
+            case .invalidResponse: return "Invalid response"
+            case .noData: return "No data"
+            case .serializationError: return "Failed to decode data"
+        }
+    }
+    var errorUserInfo: [String : Any]{
+        [NSLocalizedDescriptionKey: localizedDescription]
+    }
+}
